e4ee9739ec301e6cb11129d44f44c53179836269
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-13 23:52:49 +0330

    fix: responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
-import { NextPage } from 'next';
-import { MetaTags } from '@components';
-import styles from './index.module.scss';
+import { NextPage } from "next";
+import { MetaTags } from "@components";
+import styles from "./index.module.scss";
 
 const Home: NextPage<any> = (payload) => {
   return (
     <>
       <MetaTags
-        description='Aims to provide a panel to assist developers in managing their private servers. tools to create backups, setup docker apps, and more'
-        pageUrl='/'
+        description="Aims to provide a panel to assist developers in managing their private servers. tools to create backups, setup docker apps, and more"
+        pageUrl="/"
       />
 
-      <section className={styles.intro}>
-        <div className='container'>
+      <section className={styles.info}>
+        <div className="container">
           <section>
             <h1>Manage your servers</h1>
             <h2>Like Aliens!</h2>
@@ -25,15 +25,15 @@ const Home: NextPage<any> = (payload) => {
               One click install, pre configured and secure app deployments!
             </p>
 
-            <button>Getting started</button>
+            <a href="https://panel.shellops.io">Continue to panel</a>
           </section>
-          <img src='/assets/images/aliens.svg' alt='' />
+          <img src="/assets/images/aliens.svg" alt="" />
         </div>
       </section>
 
       <section className={styles.mock}>
-        <div className='container'>
-          <img src='/assets/images/mock.svg' alt='' />
+        <div className="container">
+          <img src="/assets/images/mock.svg" alt="" />
         </div>
       </section>
     </>
